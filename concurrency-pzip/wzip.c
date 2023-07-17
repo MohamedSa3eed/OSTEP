4cd1440789e48f1f67906e5da199c7c6da23297f
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-07-18 01:04:43 +0300

    implemented the encoding function

--- concurrency-pzip/wzip.c
+++ concurrency-pzip/wzip.c
@@ -9,9 +9,26 @@
 #include "pthread.h"
 
 #define MAX_FILES 100 // max number of files to compress
+#define MAX_CHUNKS 100 // max number of chunks to compress
+
+typedef struct {
+  int id;
+  int size;
+  int start;
+  int end; 
+} chunk;
+
+char *file = NULL ;
+
+chunk *chunks[MAX_CHUNKS] ;
+int fill = 0 ;
+int use = 0 ;
+int count = 0 ;
 
 void print_err(char *err);
 void map_files(int num_files , char *filenames[] , int *new_n , char **file);
+void enqueue(chunk *c);
+chunk *dequeue(void);
 
 int main(int argc, char *argv[])
 {
@@ -20,7 +37,6 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   int file_size = 0 ;
-  char *file = NULL ;
   map_files(argc , argv , &file_size , &file) ;
   int threads = get_nprocs();
 
@@ -56,7 +72,7 @@ void map_files(int num_files , char *filenames[] , int *new_n , char **file)
   // Map each file to memory
   off_t offset = 0;
   for (int i = 1; i < num_files; i++) {
-      file_data[i] = mmap(NULL, file_sizes[i], PROT_READ, MAP_SHARED, fds[i], 0);
+      file_data[i] = mmap(NULL, file_sizes[i], PROT_READ, MAP_PRIVATE, fds[i], 0);
       if (file_data[i] == MAP_FAILED) {
           print_err("wzip: cannot map file\n") ;
           exit(EXIT_FAILURE);
@@ -81,4 +97,41 @@ void map_files(int num_files , char *filenames[] , int *new_n , char **file)
   // update the argunemnts 
   *file = concatenated_data ;
   *new_n = (int)total_size ; 
+}
+void enqueue(chunk *c)
+{
+  chunks[fill] = c ;
+  fill = (fill + 1) % MAX_CHUNKS ;
+  count++ ;
+}
+chunk *dequeue(void)
+{
+  chunk *c = chunks[use];
+  use = (use + 1) % MAX_CHUNKS ;
+  count-- ;
+  return c;
+}
+void *producer (char *file)
+{
+  return NULL;
+}
+void *consumer(void *arg)
+{
+  return NULL;
+}
+void encode_chunk (chunk *c)
+{
+  int counter = 1 ;
+  int cursor  = 0 ;
+  while (cursor < c->size) {
+    int offset = (c->start)+cursor ;
+    if (file[offset] == file[offset+1]) {
+      counter++;
+    }
+    else {
+      // store file[offset] & counter
+      counter = 1 ;
+    }
+    cursor++ ;
+  }
 }
